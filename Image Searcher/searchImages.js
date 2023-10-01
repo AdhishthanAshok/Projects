062c6cf12534537622b1329e7c2e78678d3dc27e
Adhishthan <adhishthanashok@gmail.com>
2023-10-01 20:25:32 +0530

    Made my first commit in Projects Repo

---
+++ Image Searcher/searchImages.js
@@ -0,0 +1,21 @@
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results();
+
+  if (page == 1) {
+    searchResults.innerHTML = "";
+  }
+  results.map((results) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("img");
+  });
+}
